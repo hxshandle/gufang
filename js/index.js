d3b7df87c379740e5f6344499e555e17a5a5b249
Handle (Xiaosheng) Huang <hhuang@successfactors.com>
2014-02-08 17:47:20 +0800

    create index page

---
+++ js/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by handle on 2/8/14.
+ */
+(function ($) {
+  var vh = $(window).height(),
+      vw = $(window).width();
+
+  $('#start').click(function () {
+    alert("ok");
+  });
+
+})(jQuery);
+
