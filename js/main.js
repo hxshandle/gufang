c893ca7c2cfe31b7d7b5ab4207805348398ac905
Handle (Xiaosheng) Huang <hhuang@successfactors.com>
2014-02-13 18:11:19 +0800

    fix bg position and performance

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-(function(){
-  $('.menu').jMenu();
+(function() {
+	$('.menu').jMenu();
+	// Handle menu bg change
+	$('.menu a').hover(function(event) {
+		var $this = $(this);
+		var bgEl = $this.data('bg');
+		if (bgEl) {
+      $('.div-bg.active').removeClass('active');
+      $(bgEl).addClass('active');
+		}
+	});
 })();
+
