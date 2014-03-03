1e94732976a6ca4080a6f45d0b0e7b64f7a396dd
hxshandle <handle.huang0409@gmail.com>
2014-03-03 20:54:52 +0800

    add menu for pages

--- js/layout.js
+++ js/layout.js
@@ -1,10 +1,31 @@
 var VH = $(window).height(),
-    offset = 159;
-function initLayout(minHeight){
-  var vh = VH < minHeight ? minHeight:VH;
-  var _h = vh-159;
-  $('.main').css({height:_h+'px'});
-  $('.main .left,.main .right').css({height:_h+'px'});
-  $('.cont').css({paddingTop:(_h-360)/2+'px'});
-
+offset = 159;
+function initLayout(minHeight) {
+	var vh = VH < minHeight ? minHeight: VH;
+	var _h = vh - 159;
+	$('.main').css({
+		height: _h + 'px'
+	});
+	$('.main .left,.main .right').css({
+		height: _h + 'px'
+	});
+	$('.cont').css({
+		paddingTop: (_h - 360) / 2 + 'px'
+	});
 }
+
+// init sub-menu for pages
+$(function() {
+	var _sel = ".top-menu .has-sub-menu";
+	$(_sel).hover(
+	function() {
+		var $this = $(this);
+		$this.find('.sub-menu').slideDown();
+	},
+	function() {
+		var $this = $(this);
+		$this.find('.sub-menu').slideUp();
+	});
+
+});
+
