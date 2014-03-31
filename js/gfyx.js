e8db4d29f0d9586d59e5b30f5c604cba282f88a8
Handle (Xiaosheng) Huang <hhuang@successfactors.com>
2014-03-31 21:03:50 +0800

    ab

---
+++ js/gfyx.js
@@ -0,0 +1,21 @@
+$(function() {
+	$('.logo').bind('load', function() {
+		var $this = $(this);
+		$this.css({
+			display: 'block',
+			opacity: 0
+		}).center({
+			against: 'parent'
+		}).css({
+			right: '100px',
+			left: 'auto'
+		});
+		TweenLite.fromTo(this, 5, {
+			opacity: 0
+		},
+		{
+			opacity: 1
+		});
+	});
+});
+
---
+++ js/x2hm.js
@@ -0,0 +1,18 @@
+$(function() {
+	$('.logo').bind('load', function() {
+		var $this = $(this);
+		$this.css({
+			display: 'block',
+			opacity: 0,
+			left: '35px',
+			top: '30%' 
+		});
+		TweenLite.fromTo(this, 5, {
+			opacity: 0
+		},
+		{
+			opacity: 1
+		});
+	});
+});
+
